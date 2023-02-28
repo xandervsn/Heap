348e9b29c762d5f4ba8d89d0e33a49739c621314
Xander Siruno-Nebel <73612166+xandervsn@users.noreply.github.com>
2023-02-28 14:28:12 -0800

    add, print, still need pop (checking)

--- main.cpp
+++ main.cpp
@@ -7,10 +7,11 @@ int getParent(int);
 int getLeft(int);
 int getRight(int);
 int* add(int, int*);
+int* pop(int*, int);
 int* checkAdd(int, int*, int);
 int* clear(int*);
 int getLast(int*);
-void print(int*);
+void print(int*, int);
 
 int main(){
   int* tree = new int[101];
@@ -23,45 +24,72 @@ int main(){
       cout << "Add by user (U) or file input (F)?" << endl;
       cin >> input;
       if(strcmp(input, "U") == 0){
-	int uin;
-	cin >> uin;
-	tree = add(uin, tree);
-	//cout << tree[1] << endl;
+        int uin;
+        cin >> uin;
+        tree = add(uin, tree);
+        //cout << tree[1] << endl;
       }else if(strcmp(input, "F") == 0){
 
       }
     }else if(strcmp(input, "DLT") == 0){
-      
+      tree = pop(tree, 1);
     }else if(strcmp(input, "DLTALL") == 0){
 
     }else if(strcmp(input, "PRINT") == 0){
-      print(tree);
+      cout << "(" << tree[1] << ")" << endl;
+      print(tree, 1);
     }
   }
   return 0;
 }
 
-void print(int* tree){
-  cout << tree[1] << endl;
-  cout << tree[2] << ", " << tree[3] << endl;
+void print(int* tree, int i){
+  int last = getLast(tree);
+  int left = getLeft(i);
+  int right = getRight(i);
+  cout << "(";
+  if(tree[left] > -1){
+    cout << tree[left] << ",";
+  }
+  if(tree[right] > -1){
+    cout << tree[right];
+  }
+  cout << ") ";
+
+  if(tree[left] > -1 && tree[right] > -1){
+    print(tree, left);
+    print(tree, right);
+  }
 }
 
-int* checkAdd(int num, int* tree, int i){
-  int parent = getParent(i);
-  cout << "0: " << parent << endl;
-  cout << "1: " << tree[parent] << endl;
-  cout << "2: " << tree[i] << endl;
-  if(parent = 1){
+int* pop(int* tree, int i){
+  if(getLeft(1) == -1 && getRight(1) == -1){
+    tree[1] = -1;
     return tree;
   }
-  if(tree[parent] > num){
-    //if the parent is greater than the child (or it's the 1st input), we're in the right place
+  if(i == 1){
+    int last = getLast(tree);
+  }
+  int parent = getParent(last);
+  int parentval = tree[parent];
+  int grandparent = getParent(parent);
+  tree[last] = -1;
+  tree[parent] = tree[last];
+}
+
+int* checkAdd(int num, int* tree, int child){
+  int parent = getParent(child);
+  if(child == 1){
+    return tree;
+  }
+  if(tree[parent] > tree[child]){
+    //if the parent is greater than the child (or it's the 1st input), all is well
     return tree;
   }else{
     //if it's not, swap them and recurse
     int temp = tree[parent];
     tree[parent] = num;
-    tree[i] = temp;
+    tree[child] = temp;
     checkAdd(num, tree, parent);
   }
 }
